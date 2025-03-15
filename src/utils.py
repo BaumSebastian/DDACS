03898928602dca8264b2a759bb20da3e575e16c3
sebastian baum <baum.sebastian@proton.me>
2025-03-15 18:20:28 +0100

    renamed the varibales

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,15 @@
 from darus import Dataset as DarusDataset
 
 
-def download_dataset(url: str, root: str) -> None:
+def download_dataset(dataset_url: str, data_dir: str) -> None:
     """
     Download a dataset from a given URL and save it to a local directory.
 
-    :param url: The url to the dataset on DaRUS.
-    :type url: str
-    :param root: The local directory to save the dataset.
-    :type root: str
+    :param dataset_url: The url to the dataset on DaRUS.
+    :type dataset_url: str
+    :param data_dir: The root directory to save the dataset.
+    :type data_dir: str
     """
-    ds = DarusDataset(url)
+    ds = DarusDataset(dataset_url)
     ds.summary()
-    ds.download(root)
+    ds.download(data_dir)
