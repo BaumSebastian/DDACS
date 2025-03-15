2a55168827c38751ad47ce3ce1a2c8240436a20c
sebastian baum <baum.sebastian@proton.me>
2025-03-15 13:15:15 +0100

    reformatted using black

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import h5py
 import numpy as np
 from src import BaseFEMDataset, download_dataset
 
+
 def main():
 
     config_file_path = r"./config/config_template.yaml"
@@ -16,17 +17,29 @@ def main():
 
     if download:
         download_dataset(url, root)
-    
+
     bds = BaseFEMDataset(root, data_dir)
     print(bds)
 
     sim_id, metadata, h5_file_path = next(iter(bds))
-    print("\n".join(["Sampel data entry.", f" - ID: {sim_id}", f" - Metadata: {metadata}", f" - h5 file path: {h5_file_path}"]))
+    print(
+        "\n".join(
+            [
+                "Sampel data entry.",
+                f" - ID: {sim_id}",
+                f" - Metadata: {metadata}",
+                f" - h5 file path: {h5_file_path}",
+            ]
+        )
+    )
 
     # Access the indvidual entry based on h5 structure.
     with h5py.File(h5_file_path, "r") as f:
-            data = np.array(f["OP10"]["blank"]["node_displacement"])
-    print(f"Example of pointcloud of 'blank' gemonetry for all (4) timesteps {data.shape}")
+        data = np.array(f["OP10"]["blank"]["node_displacement"])
+    print(
+        f"Example of pointcloud of 'blank' gemonetry for all (4) timesteps {data.shape}"
+    )
+
 
 if __name__ == "__main__":
     main()
