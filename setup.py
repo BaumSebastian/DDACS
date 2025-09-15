e26a7d400d8cd08a7c52c3eba34ea87f53f0b8cd
BaumSebastian <Sirbaum@protonmail.com>
2025-09-15 13:26:12 +0200

    Release v1.0.1: Add extract_point_springback function and improve documentation

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ddacs",
-    version="1.0.0",
+    version="1.0.1",
     author="Sebastian Baum",
     author_email="Sebastian.Baum@ias.uni-stuttgart.de",
     description="Deep Drawing and Cutting Simulations (DDACS) Dataset - Python interface",
