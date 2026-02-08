47b7caec7cbe3d3ccda315a478c88a403b8fbeb0
Sebastian Baum <Baum.Sebastian@protonmail.com>
2026-02-08 16:06:12 +0100

    added docstring and included other utils.

--- ddacs/__init__.py
+++ ddacs/__init__.py
@@ -1,3 +1,19 @@
+"""
+DDACS - Deep Drawing and Cutting Simulations Dataset.
+
+A Python package for accessing and processing the DDACS dataset containing
+FEM simulations of sheet metal forming processes.
+
+Examples:
+    >>> from ddacs import iter_ddacs, count_available_simulations
+    >>> count = count_available_simulations('./data')
+    >>> for sim_id, metadata, h5_path in iter_ddacs('./data'):
+    ...     print(f"Simulation {sim_id}")
+
+    >>> from ddacs.utils import extract_mesh, extract_element_thickness
+    >>> vertices, triangles = extract_mesh('simulation.h5', 'blank')
+"""
+
 __version__ = "2.0.0"
 
 from .generators import (
@@ -8,13 +24,43 @@ from .generators import (
     count_available_simulations,
 )
 
+from .utils import (
+    extract_point_cloud,
+    extract_mesh,
+    extract_element_thickness,
+    extract_element_stress,
+    extract_element_strain,
+    extract_point_springback,
+    compute_von_mises,
+    non_degenerate_mask,
+    display_structure,
+)
+
 # Optional PyTorch dataset (only if PyTorch is available)
 try:
     from .pytorch import DDACSDataset
 except ImportError:
     pass
 
-try:
-    from . import utils
-except ImportError:
-    pass
+__all__ = [
+    # Version
+    "__version__",
+    # Generators
+    "iter_ddacs",
+    "iter_h5_files",
+    "get_simulation_by_id",
+    "sample_simulations",
+    "count_available_simulations",
+    # Utils
+    "extract_point_cloud",
+    "extract_mesh",
+    "extract_element_thickness",
+    "extract_element_stress",
+    "extract_element_strain",
+    "extract_point_springback",
+    "compute_von_mises",
+    "non_degenerate_mask",
+    "display_structure",
+    # PyTorch (optional)
+    "DDACSDataset",
+]
